04d05935da27768b3cef0ff74bc6654726acbdde
AbbieN <52113711+Abbie-n@users.noreply.github.com>
2021-08-31 13:30:19 +0100

    added fonts, color and theme classes

---
+++ lib/shared/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryBlue = Color(0xFF042E5F);
+  static const white = Colors.white;
+  static const textGrey = Color(0XFFBFC0C5);
+  static const lightBlue = Color(0XFF1043F0);
+  static const black = Color(0XFF000000);
+  static const darkTextGrey = Color(0XFF202020);
+  static const containerColor = Color(0XFFE4E4E4);
+}
---
+++ lib/shared/utils/themes.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+  fontFamily: 'Cairo',
+  textTheme: TextTheme(
+    headline1: TextStyle().copyWith(fontFamily: 'Cairo'),
+    headline2: TextStyle().copyWith(fontFamily: 'Cairo'),
+    headline4: TextStyle().copyWith(fontFamily: 'Cairo'),
+    headline3: TextStyle().copyWith(fontFamily: 'Cairo'),
+    bodyText2: TextStyle().copyWith(fontFamily: 'Cairo'),
+    bodyText1: TextStyle().copyWith(fontFamily: 'Cairo'),
+    headline5: TextStyle().copyWith(fontFamily: 'Cairo'),
+    headline6: TextStyle().copyWith(fontFamily: 'Cairo'),
+    caption: TextStyle().copyWith(fontFamily: 'Cairo'),
+    button: TextStyle().copyWith(fontFamily: 'Cairo'),
+    overline: TextStyle().copyWith(fontFamily: 'Cairo'),
+    subtitle1: TextStyle().copyWith(fontFamily: 'Cairo'),
+    subtitle2: TextStyle().copyWith(fontFamily: 'Cairo'),
+  ),
+);
